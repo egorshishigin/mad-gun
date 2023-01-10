a471b148e61dac50b40c95d33f9e6ee4b7a27a2e
Egor Shishigin <36220657+egorshishigin@users.noreply.github.com>
2023-01-10 10:31:49 +0500

    Plugins added
    -Zenject
    -DOTween
    -TMP

---
+++ madGun/Assets/Scripts/Infrastructure/BootInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Infrastructure
+{
+    public class BootInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
